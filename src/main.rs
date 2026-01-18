f1c66f1d751e1d1324d46bc2968b29ff266c83ae
rchDev <rizvan.chalilovas@proton.me>
2026-01-18 19:47:00 +0200

    add: user command reading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
 #[allow(unused_imports)]
-use std::io::{self, Write};
+use std::io::{self, Write, stdin};
+use std::process;
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap_or_else(|err| {
+        eprintln!("Failed to read user input: {err}");
+        process::exit(1);
+    });
+
+    let _command = Command::build(input).unwrap_or_else(|err| {
+        eprintln!("{err}");
+        process::exit(1);
+    });
+}
+
+struct Command {
+    name: String,
+}
+
+impl Command {
+    fn build(input: String) -> Result<Self, String> {
+        let command = input.trim();
+
+        Err(format!("{command}: command not found"))
+    }
 }
